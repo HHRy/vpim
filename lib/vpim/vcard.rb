d1a028116b05899b52e96156d7dda4a512f6fe29
Ryan Stenhouse <ryan@freeagentcentral.com>
2011-10-12 11:37:52 +0100

    Some Debuggery

--- lib/vpim/vcard.rb
+++ lib/vpim/vcard.rb
@@ -668,7 +668,13 @@ module Vpim
       else
         raise ArgumentError, "Vcard.decode cannot be called with a #{card.type}"
       end
-
+      puts "\n\n\n\n\n\n\n\n\n\n\n\n"
+      puts string.inspect
+      puts string[0].inspect
+      puts string[1].inspect
+      puts string[2].inspect
+      puts string[0,3].inspect
+      puts "\n\n\n\n\n\n\n\n\n\n\n\n"
       case string
         when Regexp.new('^\xEF\xBB\xBF', nil, 'n')
           string = string.sub("\xEF\xBB\xBF", '')
